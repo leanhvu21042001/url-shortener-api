816295c393121b8143bd31fb9da86399c5b4b482
leanhvu21042001 <leanhvu21042001@gmail.com>
2022-04-01 06:51:48 +0700

    fix: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import config from 'config';
 import express from 'express';
 import bodyParser from 'body-parser';
@@ -9,6 +10,7 @@ const app = express();
 const port = config.get('port');
 
 // middlewares
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
